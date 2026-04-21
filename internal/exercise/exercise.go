fd5389875d55e878a0c3736ed7610e624a50ab35
Gustavo Resende <gustavo.reseende@gmail.com>
2026-04-21 17:12:30 -0300

    Add MuscleGroup type and update Exercise struct with muscle group field and validation in NewExercise function

--- internal/exercise/exercise.go
+++ internal/exercise/exercise.go
@@ -1,7 +1,26 @@
 package exercise
 
+import "errors"
+
 type Exercise struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
+	ID          int         `json:"id"`
+	Name        string      `json:"name"`
+	Description string      `json:"description"`
+	MuscleGroup MuscleGroup `json:"muscle_group"`
+}
+
+func NewExercise(name string, description string, muscleGroup MuscleGroup) (*Exercise, error) {
+
+	if name == "" {
+		return nil, errors.New("name is required")
+	}
+	if muscleGroup == "" {
+		return nil, errors.New("muscle group is required")
+	}
+
+	return &Exercise{
+		Name:        name,
+		Description: description,
+		MuscleGroup: muscleGroup,
+	}, nil
 }
---
+++ internal/exercise/muscle_group.go
@@ -0,0 +1,13 @@
+package exercise
+
+type MuscleGroup string
+
+const (
+	Chest     MuscleGroup = "chest"
+	Triceps   MuscleGroup = "triceps"
+	Biceps    MuscleGroup = "biceps"
+	Back      MuscleGroup = "back"
+	Legs      MuscleGroup = "legs"
+	Shoulders MuscleGroup = "shoulders"
+	Abs       MuscleGroup = "abs"
+)
